674003b8a27e3664f573132aface2500f5de371e
bizarre84 <bizarre.san@gmail.com>
2020-09-23 16:54:51 +0300

    feat: write function carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,11 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(item) {
+  if (typeof item !== "string") return false;
+  item = parseFloat(item);
+  if (typeof item !== "number" || item !== item) return false;
+  if (item <=0 || item>=15.1) return false;
+  return (Math.ceil((Math.log(MODERN_ACTIVITY/item)/(Math.log(2)/HALF_LIFE_PERIOD))));
+  
 };
